bffbb72daa415a866347b0d0a5aa01b4acdd9907
Thanh <thanhnguyen21294@gmail.com>
2019-07-23 21:04:51 +0700

    buoi 2

---
+++ session 2/js2_1.js
@@ -0,0 +1,61 @@
+// let x = 'Hello World!'; 
+// let y = 100;
+
+// const pi = 3.14; // khai bao hang so pi
+// let xinChao = 'Hello!';
+// let XinChao = 'HELLO!';
+
+// var welcome = 'Welcome to JavaScript!'
+
+// console.log(x);
+// console.log(y);
+
+// console.log(pi);
+// console.log(xinChao);
+// console.log(XinChao);
+
+// console.log(welcome);
+
+// pi = 3;
+
+// y = 20;
+// x = 10;
+
+
+// var z = x + y;
+// var i = x * y;
+// var p = x % y; // % là phép chia lấy dư
+// var w = x / y; // / là phép chia lấy thương
+// console.log("Gia tri cua z la: " + z); // dấu + của chuỗi
+// console.log("Gia tri cua i la: " + i);
+// console.log(p); // hiển thị số dư
+// console.log(w);
+
+
+// var x = 50;
+// let m = 100;
+{// block code
+    // let x = 20; // let chỉ có tác dụng trong 1 block
+    // console.log(x);
+    // var y = 10; // var tác dụng cho toàn bộ block
+    // console.log(y);
+    // let m = 10;
+    // console.log(m);
+}
+// console.log(x);
+// console.log(y);
+// console.log(m);
+
+// let x = 100;
+// {
+//     var x = 10;
+//     console.log(x);
+    
+// }
+// console.log(x);
+// tất cả các giá trị khai báo theo kiểu var sẽ được đưa lên đầu
+
+// x = 100;
+// console.log(x);
+// var x;
+// let x; // xảy ra lỗi vì var sẽ lên trước
