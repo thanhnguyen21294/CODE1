a05855185b1a0e1cefc948054af93ebf073d1892
Thanh <thanhnguyen21294@gmail.com>
2019-08-04 02:15:23 +0700

    x

--- Bai tap/Session 5 Điều kiện/s5.js
+++ Bai tap/Session 5 Điều kiện/s5.js
@@ -1,2 +1,55 @@
 //B1: Nhập vào 1 năm, kiểm tra xem năm đó có bao nhiêu ngày?
+// let N = Number(prompt("Nhap vao 1 nam"));
+// if (N%4 == 0) {
+//     console.log(`nam ${N} co 366 ngay`);
+    
+// }else{
+//     console.log(`nam ${N} co 365 ngay`);
+    
+// }
+
+
+
+//B2: Nhập vào 1 tháng trong năm, in ra màn hình số ngày của tháng đó (Yêu cầu kiểm tra cả năm nhuận).
+// let year = Number(prompt("Nhap 1 nam bat ky"));
+// let month = Number(prompt("Nhap 1 thang bat ky"));
+// if (((year%4 == 0)||(year%4 !=="0")) && (month ==1||month==3||month==5||month==7||month==8||month==10||month==12)) {
+//     console.log(`thang ${month} cua nam ${year} co 31 ngay`);
+    
+// } else if ((year%4 == 0||(year%4 !=="0")) && (month ==4||month==6||month==9||month==11)) {
+//     console.log(`thang ${month} cua nam ${year} co 30 ngay`);
+    
+// } else if ((year%4 == 0) && (month == 2)) {
+//     console.log(`thang ${month} cua nam ${year} co 28 ngay`);
+    
+// } else{
+//     console.log(`thang ${month} cua nam ${year} co 29 ngay`);
+    
+// }
+
+
+
+//B3: Nhập vào 1 tháng trong năm, in ra mùa của tháng đó
+// let N = Number(prompt("Nhap vao 1 thang"));
+// if (N<=4 && N>=1) {
+//     console.log(`thang ${N} la mua xuan`);
+    
+// } else if (N>=5 && N<=7) {
+//     console.log(`thang ${N} la mua ha`);
+    
+// } else if (N>=8 && N<=10) {
+//     console.log(`thang ${N} la mua thu`);
+    
+// } else if (N==11 || N==12) {
+//     console.log(`thang ${N} la mua dong`);
+    
+// } else{
+//     alert(`Khong hop le`);
+    
+// }
+    
+
+
+//B4: Nhập vào 3 số a, b, c. In ra theo thứ tự tăng dần.
+
 
