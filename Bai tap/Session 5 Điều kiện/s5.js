d6bed640faf5c37f25f8a74010d8d15af9e592bf
Thanh <thanhnguyen21294@gmail.com>
2019-08-05 16:48:08 +0700

    x

--- Bai tap/Session 5 Điều kiện/s5.js
+++ Bai tap/Session 5 Điều kiện/s5.js
@@ -20,11 +20,13 @@
 //     console.log(`thang ${month} cua nam ${year} co 30 ngay`);
     
 // } else if ((year%4 == 0) && (month == 2)) {
+//     console.log(`thang ${month} cua nam ${year} co 29 ngay`);
+    
+// } else if ((year%4 !== 0) && (month == 2)){
 //     console.log(`thang ${month} cua nam ${year} co 28 ngay`);
     
 // } else{
-//     console.log(`thang ${month} cua nam ${year} co 29 ngay`);
-    
+//     alert("Khong hop le!");
 // }
 
 
@@ -51,5 +53,83 @@
 
 
 //B4: Nhập vào 3 số a, b, c. In ra theo thứ tự tăng dần.
+// let a = Number(prompt("Nhap vao 1 so "));
+// let b = Number(prompt("Nhap vao 1 so "));
+// let c = Number(prompt("Nhap vao 1 so "));
+// if (a>b && b>c) {
+//     console.log(`${c} < ${b} < ${a}`);
+    
+// }else if (a>c && c>b) {
+//     console.log(`${b} < ${c} < ${a}`);
+    
+// }else if (b>a && a>c) {
+//     console.log(`${c} < ${a} < ${b}`);
+    
+// }else if (b>c && c>a) {
+//     console.log(`${a} < ${c} < ${b}`);
+    
+// }else if (c>a && a>b) {
+//     console.log(`${b} < ${a} < ${c}`);
+    
+// }else{
+//     alert("Khong the sap xep, nhap lai");
+// }
 
 
+
+//B5: Nhập vào 3 số a, b, c. Kiểm tra xem 3 số đó có lập được thành tam giác không. 
+//Nếu có, kiểm tra xem tam giác có vuông, cân hay đều k?
+// let a = Number(prompt("Nhap 1 so"));
+// let b = Number(prompt("Nhap 1 so"));
+// let c = Number(prompt("Nhap 1 so"));
+// if ((a*a+b*b==c*c || a*a+c*c==b*b || b*b+c*c==a*a) && (a>0 && b>0 && c>0)) {
+//     console.log(`Tam giac nay la tam giac vuong`);
+    
+// }else if ( a>0 && b>0 && c>0 && a!==b && a!==c && b!==c && (((a+b)>c&&(a-b)<c) || ((a+c)>b&&(a-c)<b) || ((b+c)>a&&(b-c)<a||(c-b)<a))) {
+//     console.log(`Tam giac nay la tam giac thuong`);
+    
+// }else if ((a==b || a==c || b==c) && (a>0 && b>0 && c>0) && (a!==b || a!==c || b!==c)) {
+//     console.log(`Tam giac nay la tam giac can`);
+    
+// }else if ((a==b && b==c && a==c) && (a>0 && b>0 && c>0)) {
+//     console.log(`Tam giac nay la tam giac deu`);
+    
+// }else{
+//     alert("Khong lap duoc thanh tam giac");
+// }
+
+
+
+//B6: 
+// let N = Number(prompt("Nhap vao 1 so dien (kWh)"));
+// if (N<=50 && N>0) {
+//     let N1 = 1678 * N;
+//     console.log(`So tien phai tra cho ${N.toLocaleString("fi-FI")} kWh la ${N1.toLocaleString("fi-FI")} vnd`);
+    
+// }else if (N>50 && N<=100) {
+//     let N2 = 1734 * N;
+//     console.log(`So tien phai tra cho ${N.toLocaleString("fi-FI")} kWh la ${N2.toLocaleString("fi-FI")} vnd`);
+    
+// }else if (N>100 && N<=200) {
+//     let N3 = 2014 * N;
+//     console.log(`So tien phai tra cho ${N.toLocaleString("fi-FI")} kWh la ${N3.toLocaleString("fi-FI")} vnd`);
+    
+// }else if (N>200 && N<=300) {
+//     let N4 = 2536 * N;
+//     console.log(`So tien phai tra cho ${N.toLocaleString("fi-FI")} kWh la ${N4.toLocaleString("fi-FI")} vnd`);
+    
+// }else if (N>300 && N<=400) {
+//     let N5 = 2834 * N;
+//     console.log(`So tien phai tra cho ${N.toLocaleString("fi-FI")} kWh la ${N5.toLocaleString("fi-FI")} vnd`);
+    
+// }else if (N>400) {
+//     let N6 = 2927 * N;
+//     console.log(`So tien phai tra cho ${N.toLocaleString("fi-FI")} kWh la ${N6.toLocaleString("fi-FI")} vnd`);
+    
+// }else{
+//     alert("Khong hop le");
+// }
+
+
+
+//B7: 
