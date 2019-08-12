4ded471f663524b8b613e437dc4f392f63fa3572
Thanh <thanhnguyen21294@gmail.com>
2019-08-12 16:26:51 +0700

    x

--- Bai tap/Session 7 Object/s7.js
+++ Bai tap/Session 7 Object/s7.js
@@ -2,17 +2,29 @@ let sinhvien = {};
 let ten = prompt("Nhap ten");
 let tuoi = Number(prompt("Nhap tuoi"));
 let que = prompt("Nhap que quan");
+
 sinhvien.ten = ten;
 sinhvien.tuoi = tuoi;
 sinhvien.que = que;
+
+
+console.log(sinhvien);
+
 let danhsachmonhoc = [];
-for (let index = 0; index < 3; index++) {
+let somonhoc = Number(prompt("Nhap so mon dang hoc"));
+if (somonhoc == 0) {
+    alert("Khong co mon nao");
+    
+}else{
+for (let index = 0; index < somonhoc; index++) {
     let monhoc = prompt("Nhap mon hoc");
     danhsachmonhoc.push(monhoc);
       
-    
+    }
 }
-sinhvien.monhoc = danhsachmonhoc; 
+sinhvien.monhoc = danhsachmonhoc;
+
+
 let danhsachKeys = "";
 for (const key in sinhvien) {
     if (sinhvien.hasOwnProperty(key)) {
@@ -22,3 +34,38 @@ for (const key in sinhvien) {
     }
 }
 console.log(danhsachKeys);
+
+let capnhat = prompt("Nhap thong tin can thay doi");
+if (sinhvien.hasOwnProperty(capnhat)) {
+    if (capnhat == 'tuoi') {
+        sinhvien[capnhat] = Number(prompt("Nhap noi dung can thay doi"));
+    }
+  else {
+    sinhvien[capnhat] = prompt("Nhap noi dung thay doi");
+    
+    }  
+}   else {
+    alert("Nhap lai");
+}
+
+
+
+let lophoc = [];
+let masolop = Number(prompt("Ban hoc lop bao nhieu"));
+let tenlop = prompt("Nhap ten lop ban dang hoc");
+lophoc.masolop = masolop;
+lophoc.tenlop = tenlop;
+sinhvien.lop = lophoc;
+
+
+let monhocthem = [];
+let themsomonhoc = prompt("Nhap so mon muon hoc them");
+for (let index = 0; index < themsomonhoc; index++) {
+    themmonhoc = prompt("Nhap ten mon hoc");
+    monhocthem.push(themmonhoc);
+    
+}
+sinhvien.hocthem = monhocthem;
+
+
+
