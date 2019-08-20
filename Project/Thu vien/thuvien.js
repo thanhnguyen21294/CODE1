8f9e466dc91f4fbf1ce9858fc8db00ec229873f3
Thanh <thanhnguyen21294@gmail.com>
2019-08-20 15:59:52 +0700

    x

--- Project/Thu vien/thuvien.js
+++ Project/Thu vien/thuvien.js
@@ -1,87 +1,202 @@
-let N = "Menu: \n" +
-        "1.Danh sách: \n" +
-        "2.Thêm sách: \n" +
-        "3.Đổi tên sách: \n" +
-        "4.Mượn sách: \n" +
-        "5.Xóa sách: \n" +
-        "6.Thoát"
-let thuvien = [];
+// let N = "Menu: \n" +
+//         "1.Danh sách: \n" +
+//         "2.Thêm sách: \n" +
+//         "3.Đổi tên sách: \n" +
+//         "4.Mượn sách: \n" +
+//         "5.Xóa sách: \n" +
+//         "6.Thoát"
+// let thuvien = [];
 
 
-while (true) {
-    let Menu = Number(prompt(N));
-    let thoat = false;
+// while (true) {
+//     let Menu = Number(prompt(N));
+//     let thoat = false;
 
-    switch (Menu) {
-        case 1:
-            if (thuvien == 0) {
-                alert("Danh sách rỗng");
+//     switch (Menu) {
+//         case 1:
+//             if (thuvien == 0) {
+//                 alert("Danh sách rỗng");
                 
-            }   else{
-                for (let index = 0; index < thuvien.length; index++) {
-                    // let sach = thuvien[index];
-                    console.log(`${index + 1} + ${thuvien[index]}`);
+//             }   else{
+//                 for (let index = 0; index < thuvien.length; index++) {
+//                     // let sach = thuvien[index];
+//                     console.log(`${index + 1} + ${thuvien[index]}`);
                     
                     
-                }
-            }
-            break;
+//                 }
+//             }
+//             break;
 
-        case 2:
-            let themsach = prompt("Nhập tên sách cần thêm");
-            if (themsach == 0) {
-                alert("Không có sách để thêm");
-            }   else{
+//         case 2:
+//             let themsach = prompt("Nhập tên sách cần thêm");
+//             if (themsach == 0) {
+//                 alert("Không có sách để thêm");
+//             }   else{
                 
-                thuvien.push(themsach);
+//                 thuvien.push(themsach);
                 
                 
-            }
-            console.log(thuvien);
-            break;
+//             }
+//             console.log(thuvien);
+//             break;
         
-        case 3:
-            let sttsachcandoi = Number(prompt("Nhập số thứ tự sách cần đổi"));
-            if (sttsachcandoi > 0 && sttsachcandoi <= thuvien.length) {
-                let tensachcandoi = prompt("Nhập tên sách mới");
-                thuvien[sttsachcandoi - 1] = tensachcandoi;
-                console.log(thuvien);
+//         case 3:
+//             let sttsachcandoi = Number(prompt("Nhập số thứ tự sách cần đổi"));
+//             if (sttsachcandoi > 0 && sttsachcandoi <= thuvien.length) {
+//                 let tensachcandoi = prompt("Nhập tên sách mới");
+//                 thuvien[sttsachcandoi - 1] = tensachcandoi;
+//                 console.log(thuvien);
                 
                 
-            } 
-                else{
-                alert("Không tìm thấy sách");
-            }
+//             } 
+//                 else{
+//                 alert("Không tìm thấy sách");
+//             }
             
-            break;
+//             break;
             
-        case 4:
+//         case 4:
             
-            break;
+//             break;
 
-        case 5:
-            let sttsachcanxoa = Number(prompt("Nhập số thứ tự sách cần xóa"));
-            if (sttsachcanxoa > 0 && sttsachcanxoa <= thuvien.length) {
-                thuvien.splice(sttsachcanxoa - 1, 1);
-            } else{
-                alert("Không tìm thấy sách cần xóa");
-            }
-            console.log(thuvien);
+//         case 5:
+//             let sttsachcanxoa = Number(prompt("Nhập số thứ tự sách cần xóa"));
+//             if (sttsachcanxoa > 0 && sttsachcanxoa <= thuvien.length) {
+//                 thuvien.splice(sttsachcanxoa - 1, 1);
+//             } else{
+//                 alert("Không tìm thấy sách cần xóa");
+//             }
+//             console.log(thuvien);
             
-            break;
+//             break;
             
-        case 6:
-                thoat = true;
-                break;
-        default:
-            alert("Nhập lại!");
-    }
-    if (thoat) {
+//         case 6:
+//                 thoat = true;
+//                 break;
+//         default:
+//             alert("Nhập lại!");
+//     }
+//     if (thoat) {
+//         break;
+//     }
+// }
+// alert("Bạn đã thoát");
+// console.log("Bạn đã thoát");
+
+
+
+
+
+let N = "Menu: \n"+
+        "1.Danh sách sách trong thư viện: \n"+
+        "2.Thêm sách: \n"+
+        "3.Cập nhật thông tin sách: \n"+
+        "4.Xóa sách: \n"+
+        "5.Mượn sách: \n"+
+        "6.Thống kê sách theo số lượng: \n"+
+        "7.Sắp xếp sách theo số lượng, đơn giá, tổng giá trị: \n"+
+        "8.Thoát \n";
+let message = Number(prompt(N));
+let danhSach = [];
+dSThuVien();
+while (true) {
+    let exit = false;
+    switch (message) {
+    
+    case 1:
+        inDSSach();
         break;
+    case 2:
+        themSachMoi();
+        break;
+    case 3:
+        capNhatTTSach();
+        break;
+
+    case 4:
+        xoaSach();
+        break;
+
+    case 5:
+        muonSach();
+        break;
+    case 6:
+        thongKeSachTheoSoLuong();
+        break;
+    case 7:
+        sapXepSach();
+        break;
+    case 8:
+        exit = true
+        break;
+    default:
+        
+}
+if (exit) {
+    break;
+}
+}
+
+function sachMacDinh(){
+    return{
+        ten: '',
+        gia: 0,
+        soluong: 0
     }
 }
-alert("Bạn đã thoát");
+
+function capNhatSach(sach, ten, gia, soluong){
+    sach.ten = ten;
+    sach.gia = gia;
+    sach.soluong = soluong;
+}
+
+function dSThuVien(){
+    let sach1 = sachMacDinh();
+    capNhatSach(sach1,"Sách 1", 10000, 20);
+    let sach2 = sachMacDinh();
+    capNhatSach(sach2,"Sách 2", 50000, 10);
+}   
+
+
+function inDSSach(dSSach){
+    if (typeof(dSSach) == 'undefined') {
+        dSSach = danhSach;
+    }
+    if (dSSach.length == 0) {
+        alert("Danh sách rỗng");
+    }else{
+        let m = "Danh sách: \n";
+        m = 'STT: Book detail';
+        for (let index = 0; index < dSSach.length; index++) {
+            const sach = dSSach[index];
+            m += '\n' + `${index+1}` + sachToString(sach);
+        }
+        alert(m);
+    }
+}
+
+function sachToString(sach){
+let s = '';
+for (const key in sach) {
+    const value = sach[key];
+    s += key + ':' + value + ',';
+    }
+    if (s.length > 0) {
+        s = s.substring(0, s.length - 2);
+    }
+    return s;
+}
+
+function thongKeSachTheoSoLuong(){
+    let danhSachSach = sort
+}
+
+
+
+
 console.log("Bạn đã thoát");
+
 
 
 
