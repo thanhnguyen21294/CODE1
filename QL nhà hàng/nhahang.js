f511f9d3470d0ea19955079f459c7e4ab660e072
Thanh <thanhnguyen21294@gmail.com>
2019-09-05 01:20:10 +0700

    x

--- QL nhà hàng/nhahang.js
+++ QL nhà hàng/nhahang.js
@@ -7,38 +7,56 @@ let monChinh1 = {
     tonggia: 300000
 }
 
-dsMonChinh.push(monChinh1);
-
-for (let index = 0; index < dsMonChinh.length; index++) {
-    
-
-let count = 0;
-var table = document.getElementById("tableRestaurant");
-var row = table.insertRow(count + 1); 
-var cell1 = row.insertCell(0);
-var cell2 = row.insertCell(1);
-var cell3 = row.insertCell(2);
-var cell4 = row.insertCell(3);
-cell1.innerHTML = dsMonChinh[count].ten;
-cell2.innerHTML = dsMonChinh[count].soluong;
-cell3.innerHTML = dsMonChinh[count].dongia;
-cell4.innerHTML = dsMonChinh[count].tonggia;
-count++;
-}
-
 let monChinh2 = {
     ten: "Phở",
     soluong: 20,
     dongia: 30000,
     tonggia: 600000
 }
-dsMonChinh.push(monChinh2);
 
-function themMon(tenmonmoi,ten, soluong, dongia, tonggia){
-    tenmonmoi.ten = ten;
-    tenmonmoi.soluong = soluong;
-    tenmonmoi.dongia = dongia;
-    tenmonmoi.tonggia = tonggia;
+let monChinh3 = {
+    ten: "Bánh mỳ",
+    soluong: 50,
+    dongia: 20000,
+    tonggia: 1000000
 }
 
+
+
+dsMonChinh.push(monChinh1,monChinh2,monChinh3);
+
+
+
+
+for (let index = 0; index < dsMonChinh.length; index++) {
+    var table = document.getElementById("tableRestaurant");
+    var row = table.insertRow(index + 1); 
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    cell1.innerHTML = dsMonChinh[index].ten;
+    cell2.innerHTML = dsMonChinh[index].soluong;
+    cell3.innerHTML = dsMonChinh[index].dongia;
+    cell4.innerHTML = dsMonChinh[index].tonggia;
+}
+    
+
+
+function updateFood(tenmonmoi, soluong, dongia){
+    tenmonmoi.ten = tenmonmoi;
+    tenmonmoi.soluong = soluong;
+    tenmonmoi.dongia = dongia;
+    tenmonmoi.tonggia = soluong*dongia;
+}
+
+function addFood(tenmonmoi, soluong, dongia){
+    document.getElementById("newFood").value = tenmonmoi;
+    document.getElementById("newAmount").value = soluong;
+    document.getElementById("newPrice").value = dongia;
+    updateFood(tenmonmoi, soluong, dongia)
+    
+    dsMonChinh.push(tenmonmoi);
+    
+}
 
