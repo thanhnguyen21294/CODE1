fa8f1b403b5db5ca9d44b0c9a0a38e42637a9d81
Thanh <thanhnguyen21294@gmail.com>
2019-09-05 11:33:55 +0700

    x

--- QL nhà hàng/nhahang.js
+++ QL nhà hàng/nhahang.js
@@ -1,4 +1,6 @@
 var dsMonChinh = [];
+var dsMonChinhThemMoi = [];
+
 // var dsMonTrangMieng = [];
 let monChinh1 = {
     ten: "Cơm",
@@ -28,6 +30,57 @@ dsMonChinh.push(monChinh1,monChinh2,monChinh3);
 
 
 
+
+    
+
+function thongtinMon(){
+    return {
+        ten: '',
+        soluong: 0,
+        dongia: 0,
+        tonggia: 0
+    }
+}
+function updateFood(food,tenMoi, soluong, dongia){
+    food.ten = tenMoi;
+    food.soluong = soluong;
+    food.dongia = dongia;
+    food.tonggia = soluong*dongia;
+}
+
+
+
+function addFood(){
+    let newFood = thongtinMon();
+    let tenmonmoi = document.getElementById("newFood").value ;
+    let soluong = document.getElementById("newAmount").value ;
+    let dongia = document.getElementById("newPrice").value ;
+    updateFood(newFood,tenmonmoi, soluong, dongia);
+    dsMonChinhThemMoi.push(newFood);
+    
+    for (let index = 0; index < dsMonChinhThemMoi.length; index++) {
+        
+        var table = document.getElementById("tableRestaurant");
+        var row = table.insertRow(index+1+dsMonChinh.length); 
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        cell1.innerHTML = dsMonChinhThemMoi[index].ten;
+        cell2.innerHTML = dsMonChinhThemMoi[index].soluong;
+        cell3.innerHTML = dsMonChinhThemMoi[index].dongia;
+        cell4.innerHTML = dsMonChinhThemMoi[index].tonggia;
+        
+        
+    }
+    let N = dsMonChinhThemMoi.shift();
+        // console.log(N);
+    
+}
+
+console.log(dsMonChinh);
+
+
 for (let index = 0; index < dsMonChinh.length; index++) {
     var table = document.getElementById("tableRestaurant");
     var row = table.insertRow(index + 1); 
@@ -39,24 +92,7 @@ for (let index = 0; index < dsMonChinh.length; index++) {
     cell2.innerHTML = dsMonChinh[index].soluong;
     cell3.innerHTML = dsMonChinh[index].dongia;
     cell4.innerHTML = dsMonChinh[index].tonggia;
-}
-    
-
-
-function updateFood(tenmonmoi, soluong, dongia){
-    tenmonmoi.ten = tenmonmoi;
-    tenmonmoi.soluong = soluong;
-    tenmonmoi.dongia = dongia;
-    tenmonmoi.tonggia = soluong*dongia;
-}
-
-function addFood(tenmonmoi, soluong, dongia){
-    document.getElementById("newFood").value = tenmonmoi;
-    document.getElementById("newAmount").value = soluong;
-    document.getElementById("newPrice").value = dongia;
-    updateFood(tenmonmoi, soluong, dongia)
-    
-    dsMonChinh.push(tenmonmoi);
     
 }
+
 
